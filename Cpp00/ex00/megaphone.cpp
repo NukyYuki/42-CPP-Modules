3c64259e80c7b16cf2305cac9b729a2d42fe60af
mipinhei <mipinhei@mipinhei.myguest.virtualbox.org>
2025-11-23 21:42:59 +0000

    ex00

---
+++ Cpp00/ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << "\n";
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			for (int j = 0; av[i][j]; j++)
+			{
+				std::cout << static_cast<char>(toupper(av[i][j]));
+			}
+		}
+		std::cout << "\n";
+	}
+}
