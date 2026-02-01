9755e7c4f8ca73cfd9d97023aef6089897a04a13
mipinhei <mipinhei@mipinhei.myguest.virtualbox.org>
2026-02-01 21:42:30 +0000

    Cpp06 - ex02

---
+++ Cpp06/ex02/Base.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mipinhei <mipinhei@student.42porto.com>    #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-02-01 21:17:55 by mipinhei          #+#    #+#             */
+/*   Updated: 2026-02-01 21:17:55 by mipinhei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <exception>
+
+class Base {
+	public:
+		virtual	~Base();
+};
---
+++ Cpp06/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mipinhei <mipinhei@student.42porto.com>    #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-02-01 21:17:50 by mipinhei          #+#    #+#             */
+/*   Updated: 2026-02-01 21:17:50 by mipinhei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base	*generate(){
+	
+	srand(time(NULL));
+
+	int random = rand() % 3;
+}
+
+void	identify(Base* p){
+
+}
+
+void	identify(Base &p){
+
+}
+
+int main(){
+
+}
