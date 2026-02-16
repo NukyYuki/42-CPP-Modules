71d63daa9ad635b97fe856127cf5c60387547b96
Miguel Bragança <mipinhei@student.42porto.com>
2026-02-16 15:15:55 +0000

    Cpp07 - missing mains, needs testing

---
+++ Cpp07/ex00/whatever.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+template <typename T> void swap(T &int1, T &int2){
+
+    T temp = int1;
+    int1 = int2;
+    int2 = temp;
+    return;
+}
+
+template <typename T> T max(T a, T b){
+    return (a > b ? a : b);
+}
+
+template <typename T> T min(T a, T b){
+    return (a < b ? a : b);
+}
---
+++ Cpp07/ex02/Array.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <iostream>
+#include <exception>
+#include <string>
+
+template <typename T> 
+class Array{
+    private:
+        T*              _arr;
+        unsigned int    _size;
+
+    public:
+        Array() : _arr(NULL), _size(0){
+            std::cout << "Default Array Template constructor called" << std::endl;
+        }
+
+        Array(unsigned int n) : _arr(new T[n]), _size(n){
+            std::cout << "Param Array Template constructor called" << std::endl;
+        }
+
+        Array(const Array &obj) : _size(obj._size){
+            _arr = new T[_size];
+            for(unsigned int i = 0; i < _size; i++)
+                _arr[i] = obj._arr[i];
+            std::cout << "Array Template copy constructor called" << std::endl;
+        }
+
+        Array   &operator=(const Array &obj){
+            if (this != obj)
+            {
+                delete[] _arr;
+                _size = obj._size;
+                _arr = new T[_size];
+                for(unsigned int i = 0; i < _size; i++)
+                    _arr[i] = obj._arr[i];
+            }
+            std::cout << "Array Template assignment operator called" << std::endl;
+            return (this);
+        }
+
+        T   &operator[](unsigned int index)
+        {
+            if (_size == 0)
+            {
+                throw NullArrSize();
+            }
+            if (index >= _size)
+                throw OutofBounds();
+            return (_arr[index]);
+        }
+
+        const T   &operator[](unsigned int index) const
+        {
+            if (_size == 0)
+            {
+                throw NullArrSize();
+            }
+            if (index >= _size)
+                throw OutofBounds();
+            return (_arr[index]);
+        }
+
+        ~Array(){
+            delete[] _arr;
+            std::cout << "Array Template destructor called" << std::endl;
+        }
+
+        unsigned int size() const
+	    {
+	    	return (this->_size);
+	    }
+
+        class OutofBounds : public std::exception
+        {
+            public:
+            virtual const char* what() const throw(){
+                return("Index is out of range\n");
+            }
+        };
+
+        class NullArrSize : public std::exception
+        {
+            public:
+            virtual const char* what() const throw(){
+                return("The Array is empty\n");
+            }
+        };
+};
